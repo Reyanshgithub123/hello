f42433d26ad55c8a32779c5934fcf400afbd7eb0
Reyanshgithub123 <reyanshsidha1@gmail.com>
2025-01-29 17:10:30 +0530

    backend

---
+++ models/userModal.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+  isAvatarImageSet: {
+    type: Boolean,
+    default: false,
+  },
+  avatarImage: {
+    type: String,
+    default: "",
+  },
+});
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+module.exports = mongoose.model("Users", userSchema); 
---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const {register, login,getAllUsers,setAvatar,logOut} =require('../controllers/usercontroller')
+
+const router=require("express").Router()
+
+router.post("/register",register)
+
+router.post("/login",login)
+router.get("/allusers/:id", getAllUsers);
+router.post("/setavatar/:id", setAvatar);
+router.get("/logout/:id", logOut);
+
+module.exports=router
+
